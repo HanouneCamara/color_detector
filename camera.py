18bba2ebe91c14e76e4fd25fb6cabf8f019b9cfe
HanouneCamara <Alicmr13@gmail.com>
2025-05-13 15:20:18 +0100

    open_camera

---
+++ camera.py
@@ -0,0 +1,19 @@
+import cv2
+
+#ouvre la webcam
+def open_camera(index=0):
+    cap = cv2.VideoCapture(index, cv2.CAP_V4L2)
+    if not cap.isOpened():
+        raise IOError("Impossible d'ouvrir la caméra")
+    return cap
+
+#lire une image depuis la caméra
+def read_frame(cap):
+    ret, frame = cap.read()
+    return ret, frame
+
+#fermer la caméra
+def close_camera(cap):
+    cap.release()
+    cv2.destroyAllWindows()
+    
---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+from camera import open_camera, read_frame, close_camera
+
+def main():
+    try:
+        cap = open_camera()
+
+        cv2.namedWindow("Flux Vidéo", cv2.WINDOW_NORMAL)
+
+        while True:
+            ret, frame = read_frame(cap)
+            if not ret:
+                print("Erreur lors de la lecture de l'image")
+                break
+
+            cv2.imshow("Flux Vidéo", frame)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+
+    except Exception as e:
+        print(f"Erreur : {e}")
+    finally:
+        close_camera(cap)
+
+if __name__ == "__main__":
+    main()
